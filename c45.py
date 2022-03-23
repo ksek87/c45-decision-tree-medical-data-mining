30a1ad87f0b1df3af2c53a11f21406d7a6b1f7b6
Keelin Sekerka <59572821+ksek87@users.noreply.github.com>
2022-03-23 13:33:05 -0300

    skeleton for tree and node classes

--- c45.py
+++ c45.py
@@ -8,3 +8,48 @@
     References Consulted:
     [1] Data Mining (3rd Edition) Chapter 8 https://doi-org.ezproxy.library.dal.ca/10.1016/B978-0-12-381479-1.00008-3
 '''
+
+import numpy as np
+import pandas as pd
+import itertools
+import math
+
+
+class Node:
+    def __init__(self, x, y, node_type):
+        self.data = x
+        self.labels = y
+        self.attributes_list = list(self.data.columns)
+        self.best_attribute_split = None
+        self.node_type = node_type
+        self.leaf_label = None
+        self.depth = 0
+
+    def predict_leaf_class(self):
+        # takes frequency of classes in D to determine the majority class to set as output leaf label
+        return
+
+class C45Tree:
+    def __init__(self):
+        self.tree_nodes = []
+        self.depth = 0
+        self.num_leaves = 0
+        return
+
+    def grow_tree(self):
+        # generates the decision tree recursively
+        return
+    def predict(self, test_data):
+        # uses test set to predict class labels from the constructed tree
+        return
+    def attribute_selection_method(self, node):
+        return
+
+    def information_gain(self):
+        return
+
+    def information_gain_ratio(self):
+        return
+
+    def print_tree(self):
+        return

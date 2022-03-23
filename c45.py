99975942078e26e115feb275740bcd6a5f494ba5
Keelin Sekerka <59572821+ksek87@users.noreply.github.com>
2022-03-23 13:06:39 -0300

    added header

---
+++ c45.py
@@ -0,0 +1,10 @@
+'''
+    CSCI 4144 Data Mining and Warehousing - Project
+    Title: Implementing C4.5 Decision Tree Algorithm for Medical Data Mining
+    Author: Keelin Sekerka-Bajbus B00739421
+
+    Program Description:
+
+    References Consulted:
+    [1] Data Mining (3rd Edition) Chapter 8 https://doi-org.ezproxy.library.dal.ca/10.1016/B978-0-12-381479-1.00008-3
+'''
